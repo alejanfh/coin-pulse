2505280704515bc539eae5f182155a2d05c9a492
Alejandro <alejanfh@gmail.com>
2026-02-12 13:43:56 +0100

    add coin and trending overview + suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-const Page = () => {
-  return <div></div>;
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import { Suspense } from "react";
+
+const Page = async () => {
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<div>Loading...</div>}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
---
+++ components/DataTable.tsx
@@ -0,0 +1,71 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+import { DataTableProps } from "@/type";
+
+const DataTable = <T,>({
+  columns,
+  data,
+  rowKey,
+  tableClassName,
+  headerRowClassName,
+  headerCellClassName,
+  bodyRowClassName,
+  bodyCellClassName,
+  headerClassName,
+}: DataTableProps<T>) => {
+  return (
+    <Table className={cn("custom-scrollbar", tableClassName)}>
+      <TableHeader className={headerClassName}>
+        <TableRow className={cn("hover:bg-transparent!", headerRowClassName)}>
+          {columns.map((column, i) => (
+            <TableHead
+              key={i}
+              className={cn(
+                "bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5",
+              )}
+            >
+              {column.header}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((row, rowIndex) => (
+          <TableRow
+            key={rowKey(row, rowIndex)}
+            className={cn(
+              "overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/3! relative",
+              bodyRowClassName,
+            )}
+          >
+            {columns.map((column, columnIndex) => (
+              <TableCell
+                key={columnIndex}
+                className={cn("py-4 first:pl-5 last:pr-5")}
+              >
+                {column.cell(row, rowIndex)}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+};
+
+export default DataTable;
